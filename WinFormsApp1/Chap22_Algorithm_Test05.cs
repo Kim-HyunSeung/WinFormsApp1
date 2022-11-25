f413a3c15bae978d5dbe84fa5b630d5b0f7c086b
admin <admin@DESKTOP-F77F98F>
2022-11-25 17:46:30 +0900

    dsfdfs

--- WinFormsApp1/Chap22_Algorithm_Test05.cs
+++ WinFormsApp1/Chap22_Algorithm_Test05.cs
@@ -14,30 +14,18 @@ namespace CHAP04_MiddleExam
         private void button1_Click(object sender, EventArgs e)
         {
 
-            string M1;//메시지 박스에 담을 그릇
-            string M2;
-            int M3;
-            string M4;
-            string sTitle = lblTitle.Text; //라벨의 문자열변수에 넣어줌
-
-            string[]s1 = { "{","}"," " };  //바뀔 문자
-            string[]s2 = { "" ,"",""};  // 바뀐문자
-
-           
-            M1 = sTitle.Replace(s1[0], s2[0]);
-            M2 = M1.Replace(s1[1], s2[1]);
-            M4 = M2.Replace(s1[2], s2[2]);
-            MessageBox.Show(M4);           //이렇게 할필요 없고 편리한 방법있음.
-
-            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ요까지가 라벨 양쪽 괄호 없앤부분 이밑에부터 ㄱ
+            string sTitle = lblTitle.Text;
+            sTitle = sTitle.Replace("{", "");
+            sTitle = sTitle.Replace("}", "");
+            sTitle = sTitle.Replace(" ", "");
 
             // 1,2,3,4,5,10,11,12
             string[] sValues = sTitle.Split(',');
 
-            // int 배열에 담기
+            // int 배열 생성 (문자 배열의 데이터 개수 만큼 방 만들기) 
             int[] iVlaues = new int[sValues.Length];
 
-
+            // 문자 배열 정수 배열로 담기.
             for (int i = 0; i < sValues.Length; i++)
             {
                 iVlaues[i] = Convert.ToInt32(sValues[i]);
@@ -46,30 +34,92 @@ namespace CHAP04_MiddleExam
             // 정수 배열 정렬
             Array.Sort(iVlaues);
 
-            for (int i = 0; i<iVlaues.Length; i++)
-                {
-                if()
-                {
+            //1
+            //2
+            //3
+            //8
+            //8
+            //8
+            //13
+            //13
+            //15
+            //15
+            //17
+            //19   
+            //23
 
 
+
+            // 중복 값을 찾은 횟수 Count
+            int iFindCount = 0;
+
+            // 찾은 값을 누적 시킬 변수
+            string sValue = string.Empty;
+
+            // 찾은 이전 값 을 저장 시킬 변수. 
+            int iFindValue = 0;
+
+            // int i : 배열의 데이터 주소
+            for (int i = 0; i < iVlaues.Length; i++)
+            {
+                // 배열의 마지막 데이터 인지 확인 ( 비교할 다음 데이터 가 없음 )
+                if (i + 1 == iVlaues.Length)
+                {
+                    break;
                 }
 
+                // 지금 데이터와 다음 데이터 가 같은지 확인. 
+                if (iVlaues[i] == iVlaues[i + 1])
+                {
 
+                    // 이전에 찾은 데이터와 다음 데이터가 같다 : 중복 데이터가 2개 이상일 경우
+                    if (iFindValue == iVlaues[i + 1]) continue;
+
+                    // 중복 데이터 찾은 횟수 증가.
+                    ++iFindCount;
+
+                    // 두번째 중복 값 일 경우는 체크 하지 않음. 
+                    if (iFindCount == 2) continue;
+
+                    // 첫번째 중복 값과 세번째 중복값 을 누적
+                    sValue += Convert.ToString(iVlaues[i]) + " ";
+
+                    // 세번째 중복 값을 찾았을 경우 Break;
+                    if (iFindCount == 3) break;
+                }
+
+                // 이전 데이터 를 등록. 
+                iFindValue = iVlaues[i];
             }
 
-
-
-
-
-
-
-
-
-
-            // 다음 문자열에 포함된 수를 정수 배열로 만들고 중복 되는 수 중 낮은 수 의 첫번째 값과 세번째 값을 메세지 박스로 나타내세요.
-            // 라벨에 입력 된  문자열을 받아 정수 배열로 만드는 로직을 작성할것
-            // {1, 2 , 13, 15 , 17,  23,  8,  15, 8,  19,  3,  8,  13 }
-
+            // 첫번째 중복값 과 세번째 중복값 표현.
+            MessageBox.Show(sValue);
         }
+
+        //1
+        //2
+        //3
+        //8
+        //8
+        //8
+        //13
+        //13
+        //15
+        //15
+        //17
+        //19   
+        //23
+
+
+
+
+
+
+
+        // 다음 문자열에 포함된 수를 정수 배열로 만들고 중복 되는 수 중 낮은 수 의 첫번째 값과 세번째 값을 메세지 박스로 나타내세요.
+        // 라벨에 입력 된  문자열을 받아 정수 배열로 만드는 로직을 작성할것
+        // {1, 2 , 13, 15 , 17,  23,  8,  15, 8,  19,  3,  8,  13 }
+
     }
+
 }
